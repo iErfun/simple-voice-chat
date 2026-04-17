dfbf3f2f0279449595d0bd54b2a1356d36d14d18
iErfun <167802850+iErfun@users.noreply.github.com>
2026-04-17 12:12:29 +0330

    Update OpusManager.java

--- common/src/main/java/de/maxhenkel/voicechat/natives/OpusManager.java
+++ common/src/main/java/de/maxhenkel/voicechat/natives/OpusManager.java
@@ -16,7 +16,7 @@ public class OpusManager extends NativeValidator {
 
     @Override
     protected void runValidation() throws Throwable {
-        NativeOpusEncoderImpl encoder = new NativeOpusEncoderImpl(AudioUtils.SAMPLE_RATE, 2, Application.VOIP);
+        NativeOpusEncoderImpl encoder = new NativeOpusEncoderImpl(AudioUtils.SAMPLE_RATE, 1, Application.AUDIO);
         encoder.setMaxPayloadSize(AudioUtils.DEFAULT_MAX_PAYLOAD_SIZE);
         byte[] encoded = encoder.encode(new short[AudioUtils.FRAME_SIZE]);
         encoder.resetState();
@@ -51,7 +51,7 @@ public class OpusManager extends NativeValidator {
 
         if (instance.canUse()) {
             try {
-                NativeOpusEncoderImpl encoder = new NativeOpusEncoderImpl(AudioUtils.SAMPLE_RATE, 2, application);
+                NativeOpusEncoderImpl encoder = new NativeOpusEncoderImpl(AudioUtils.SAMPLE_RATE, 1, application);
                 encoder.setMaxPayloadSize(mtuSize);
                 return encoder;
             } catch (Throwable e) {
